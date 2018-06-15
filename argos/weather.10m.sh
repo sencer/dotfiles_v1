f1b9b50eadadc73ed5de654fa60b64410e63d699
Sencer Selcuk <sencerselcuk@gmail.com>
2018-06-15 11:31:52 -0400

    get rid of out of service message

--- argos/weather.10m.sh
+++ argos/weather.10m.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 IFS=$'\n' data=($(curl -s wttr.in/?0QT|awk '{print substr($0,16,length($0)-15)}'))
-echo ${data[1]:-_}
-echo "---"
-echo "${data[0]}|href='http://wttr.in'"
-echo "${data[1]} ${data[2]}"
-echo "${data[3]} ${data[4]}"
 
+if echo "$data"|grep -q moment; then
+  echo "_"
+  echo "---"
+  echo "Out of service|href='http://wttr.in'"
+else
+  echo ${data[1]:-_}
+  echo "---"
+  echo "${data[0]}|href='http://wttr.in'"
+  echo "${data[1]} ${data[2]}"
+  echo "${data[3]} ${data[4]}"
+fi
