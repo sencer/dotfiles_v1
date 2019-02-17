1c2046de2c1c8b53f22e988f1bf3c20fa387b6a1
Sencer Selcuk <sencerselcuk@gmail.com>
2019-02-17 13:16:32 -0500

    move to forex.1forge.com with an api key

--- argos/doviz.5m.sh
+++ argos/doviz.5m.sh
@@ -1,18 +1,7 @@
 #!/usr/bin/env bash
-function bloomberg {
-  curl -s https://www.bloomberg.com/markets/api/quote-page/${1}:CUR | jq '.["basicQuote"]|[.price, .lowPrice, .highPrice, .priceTime ]|@sh'|xargs
-}
-usd=( $(bloomberg USDTRY) )
-eur=( $(bloomberg EURTRY) )
-gld=( $(bloomberg XAUUSD) )
-echo "${usd[0]:-_}"
-echo "---"
-echo "USDTRY ${usd[0]}|font=monospace" | xargs
-echo "--Low  : ${usd[1]}|font=monospace href='https://www.bloomberg.com/quote/USDTRY:CUR'"
-echo "--High : ${usd[2]}|font=monospace href='https://www.bloomberg.com/quote/USDTRY:CUR'"
-echo "EURTRY ${eur[0]}|font=monospace" | xargs
-echo "--Low  : ${eur[1]}|font=monospace href='https://www.bloomberg.com/quote/EURTRY:CUR'"
-echo "--High : ${eur[2]}|font=monospace href='https://www.bloomberg.com/quote/EURTRY:CUR'"
-echo "GOLD   ${gld[0]}|font=monospace" | xargs
-echo "--Low  : ${gld[1]}|font=monospace href='https://www.bloomberg.com/quote/XAUUSD:CUR'"
-echo "--High : ${gld[2]}|font=monospace href='https://www.bloomberg.com/quote/XAUUSD:CUR'"
+
+url="https://forex.1forge.com/1.0.3/quotes?pairs=USDTRY,EURTRY,EURUSD&api_key" 
+data="$(curl -s "$url=$(pass 1forge)" | jq '.[]|[.symbol,.price]|@sh')"
+
+echo ${data} | awk 'NR==1{printf("%.3f\n---\n", $2);exit}'
+echo "${data}"|tr -d \'|tr -d \"
